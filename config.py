9206099f22359d65a43fdea25670be5447b37a1d
Botmaster55 <wajsarif461@gmail.com>
2026-02-27 20:17:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN"))
-MONGO_URI = os.environ.get("MONGO_URI")
-GROUP_ID = int(os.environ.get("GROUP_ID"))
-GROUP_LINK = os.environ.get("GROUP_LINK")
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+GROUP_ID = int(os.getenv("GROUP_ID", 0))
+GROUP_LINK = os.getenv("GROUP_LINK")
+
 REQUIRED_REFERRALS = 5
